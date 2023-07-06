21ff6ec9c82e2f5e632ce3d7902df0f1ca0bbadb
Vlad Epov <vladep622@gmail.com>
2023-07-06 12:41:50 +0300

    added first home component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,17 @@
-export function Home(){
+export function Home() {
 
-    return(
-        <section>
-            <h1>Hello from Home</h1>
+    return (
+        <section className="home-page-container">
+            <section className="home-page">
+                <section className="header-extension">
+                    <article className="left">
+                        <h1>Find the best <span>talent</span></h1>
+                    </article>
+                    <article className="right">
+                        <p>Finding the right people and building high performing teams can be hard. Most companies aren't tapping into the abundance of global talent. We're about to change that.</p>
+                    </article>
+                </section>
+            </section>
         </section>
     )
 }
