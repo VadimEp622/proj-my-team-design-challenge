07f36056d28139234cbdfd099751709506c5b37b
Vlad Epov <vladep622@gmail.com>
2023-10-19 18:36:20 +0300

    1. improved positioning for "home-header-extension" background svg picture 2. added 4px height, 50px width colored line above right side of cmp in desktop media-query range

--- src/cmps/home/home-header-extension.jsx
+++ src/cmps/home/home-header-extension.jsx
@@ -5,19 +5,21 @@ export function HomeHeaderExtension() {
 
     return (
         <section className="header-extension-container full main-layout">
-            <section className="bg-img bgPatternHome1 full">
-                <img src={bgPatternHome1} alt="bgPatternHome1" />
-            </section>
-            <section className="header-extension">
-                <article className="title">
-                    <h1>Find the best <span>talent</span></h1>
-                </article>
-                <article className="content">
-                    <p>Finding the right people and building high performing teams can be hard. Most companies aren't tapping into the abundance of global talent. We're about to change that.</p>
-                </article>
-            </section>
-            <section className="bg-img bgPatternHome2 full">
-                <img src={bgPatternHome2} alt="bgPatternHome2" />
+            <section className="full main-layout">
+                <section className="bg-img bgPatternHome1 full">
+                    <img src={bgPatternHome1} alt="bgPatternHome1" />
+                </section>
+                <section className="header-extension">
+                    <article className="title">
+                        <h1>Find the best <span>talent</span></h1>
+                    </article>
+                    <article className="content">
+                        <p>Finding the right people and building high performing teams can be hard. Most companies aren't tapping into the abundance of global talent. We're about to change that.</p>
+                    </article>
+                </section>
+                <section className="bg-img bgPatternHome2">
+                    <img src={bgPatternHome2} alt="bgPatternHome2" />
+                </section>
             </section>
         </section>
     )
