c585a9862ece7edd5c56f4a3fb02441a0e738b94
Vlad Epov <vladep622@gmail.com>
2023-10-25 19:44:12 +0300

    1. fixed overflow of bg-patterns which cause scroll of shame 2. added bg-pattern to side-navbar

--- src/cmps/side-navbar.jsx
+++ src/cmps/side-navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom"
 import { Context } from "../context"
 
 import iconClose from "../assets/images/icon-close.svg"
+import bgPatternMobileNav1 from "../assets/images/bg-pattern-about-1-mobile-nav-1.svg"
 
 
 export function SideNavbar({ isSideNavbar }) {
@@ -25,6 +26,9 @@ export function SideNavbar({ isSideNavbar }) {
         <section className={`side-navbar-wrapper${isSideNavbar ? ' active' : ''}`}>
             <div className="black-transparent-bg-wrapper" onClick={(ev) => onCloseSideNavbar(ev)}></div>
             <aside className="side-navbar-container">
+                <section className="bg-pattern bgPatternMobileNav1">
+                    <img src={bgPatternMobileNav1} alt="bgPatternMobileNav1" />
+                </section>
                 <section className="side-navbar">
                     <nav>
                         <ul className="nav-list">
