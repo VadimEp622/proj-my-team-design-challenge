4b9fb86e2c1627d6cd38956fbf2779d562f3dc94
Vlad Epov <vladep622@gmail.com>
2023-10-25 16:38:45 +0300

    added bg-pattern to get-started cmp

--- src/cmps/app-get-started.jsx
+++ src/cmps/app-get-started.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import bgPatternHome6 from "../assets/images/bg-pattern-home-6-about-5.svg"
 
 export function GetStarted() {
 
     return (
         <section className="get-started-container full main-layout">
+            <section className="full bg-pattern bg-pattern-home-6">
+                <img src={bgPatternHome6} alt="bgPatternHome6" />
+            </section>
             <section className="get-started">
                 <section className="title"><h2>Ready to get started?</h2></section>
                 <section className="contact-btn-container">
