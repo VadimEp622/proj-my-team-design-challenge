6949133861d723e51b0506fccb1f8a76b246ab15
Vlad Epov <vladep622@gmail.com>
2023-10-25 13:46:15 +0300

    added mobile-first approach responsiveness for home-info cmp

--- src/cmps/home/home-info.jsx
+++ src/cmps/home/home-info.jsx
@@ -7,42 +7,38 @@ export function HomeInfo() {
     return (
         <section className="info-container full main-layout">
             <section className="info">
-                <article className="left">
+                <section className="title">
                     <h2>Build & manage distributed teams like no one else.</h2>
-                </article>
-                <article className="right">
-                    <ul>
-                        <li>
-                            <section className="item">
-                                <article className="img-container">
-                                    <img src={imgPerson} alt="imgPerson"></img>
-                                </article>
-                                <article className="content-container">
-                                    <h3>Experienced Individuals</h3>
-                                    <p>Our network is made up of highly experienced professionals who are passionate about what they do.</p>
-                                </article>
-                            </section>
-                            <section className="item">
-                                <article className="img-container">
-                                    <img src={imgCog} alt="imgCog"></img>
-                                </article>
-                                <article className="content-container">
-                                    <h3>Easy to Implement</h3>
-                                    <p>Our processes have been refined over years of implementation meaning our teams always deliver.</p>
-                                </article>
-                            </section>
-                            <section className="item">
-                                <article className="img-container">
-                                    <img src={imgChart} alt="imgChart"></img>
-                                </article>
-                                <article className="content-container">
-                                    <h3>Enhanced Productivity</h3>
-                                    <p>Our customized platform with in-built analytics helps you manage your distributed teams.</p>
-                                </article>
-                            </section>
-                        </li>
-                    </ul>
-                </article>
+                </section>
+                <ul className="list">
+                    <li className="item">
+                        <section className="img-container">
+                            <img src={imgPerson} alt="imgPerson"></img>
+                        </section>
+                        <section className="content-container">
+                            <h3>Experienced Individuals</h3>
+                            <p>Our network is made up of highly experienced professionals who are passionate about what they do.</p>
+                        </section>
+                    </li>
+                    <li className="item">
+                        <section className="img-container">
+                            <img src={imgCog} alt="imgCog"></img>
+                        </section>
+                        <section className="content-container">
+                            <h3>Easy to Implement</h3>
+                            <p>Our processes have been refined over years of implementation meaning our teams always deliver.</p>
+                        </section>
+                    </li>
+                    <li className="item">
+                        <section className="img-container">
+                            <img src={imgChart} alt="imgChart"></img>
+                        </section>
+                        <section className="content-container">
+                            <h3>Enhanced Productivity</h3>
+                            <p>Our customized platform with in-built analytics helps you manage your distributed teams.</p>
+                        </section>
+                    </li>
+                </ul>
             </section>
         </section>
     )
