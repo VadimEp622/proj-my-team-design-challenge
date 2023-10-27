ee56609eb65a51c0f03c3891adaa7f71ec208ec9
Vlad Epov <vladep622@gmail.com>
2023-10-27 09:58:07 +0300

    imported responsive get-started cmp into about-page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,3 +1,4 @@
+import { GetStarted } from "../cmps/_reusable/app-get-started"
 import { AboutIntro } from "../cmps/about/about-intro"
 
 export function About() {
@@ -5,6 +6,7 @@ export function About() {
     return (
         <section className="about-page full main-layout">
             <AboutIntro />
+            <GetStarted />
         </section>
     )
 }
