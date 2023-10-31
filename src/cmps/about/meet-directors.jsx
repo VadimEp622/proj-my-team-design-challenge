08bc277f5b671d359280f8a986d2096aec5eaded
Vlad Epov <vladep622@gmail.com>
2023-10-31 19:28:26 +0200

    added meet-directors bg-pattern

--- src/cmps/about/meet-directors.jsx
+++ src/cmps/about/meet-directors.jsx
@@ -1,10 +1,14 @@
 import { DirectorList } from "./director-list"
 import { directors } from "../../services/director.service"
+import bgPatternAbout2 from "../../assets/images/bg-pattern-about-2-contact-1.svg"
 
 export function MeetDirectors() {
 
     return (
         <section className="meet-directors-container main-layout full">
+            <section className="full bg-pattern bgPatternAbout2">
+                <img src={bgPatternAbout2} alt="bgPatternAbout2" />
+            </section>
             <section className="meet-directors">
                 <section className="title"><h1>Meet the directors</h1></section>
                 <DirectorList directors={directors} />
