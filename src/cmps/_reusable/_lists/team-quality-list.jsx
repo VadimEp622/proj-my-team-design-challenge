77519df14ba8db147f05d5cd7a8edec2b8c5e076
Vlad Epov <vladep622@gmail.com>
2023-11-07 09:54:34 +0200

    added helper stylings to cmp

--- src/cmps/_reusable/_lists/team-quality-list.jsx
+++ src/cmps/_reusable/_lists/team-quality-list.jsx
@@ -3,7 +3,7 @@ import { TeamQualityPreview } from "./team-quality-preview"
 export function TeamQualityList({ teamQualities }) {
 
     return (
-        <section className="team-quality-list">
+        <section className="team-quality-list flex column">
             {
                 teamQualities.map((teamQuality, index) => <TeamQualityPreview key={index} teamQuality={teamQuality} />)
             }
--- src/cmps/_reusable/_lists/team-quality-preview.jsx
+++ src/cmps/_reusable/_lists/team-quality-preview.jsx
@@ -1,11 +1,11 @@
 export function TeamQualityPreview({ teamQuality }) {
 
     return (
-        <section className="team-quality-preview">
-            <section className="img-container">
+        <section className="team-quality-preview flex">
+            <section className="img-container no-grow-shrink">
                 <img src={teamQuality.img} alt={teamQuality.imgName}></img>
             </section>
-            <section className="content-container">
+            <section className="content-container flex column">
                 <h3 className="typography-4">{teamQuality.title}</h3>
                 <p className="typography-5">{teamQuality.content}</p>
             </section>
