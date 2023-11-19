40ba094abc727d47920f1434b48afae16b41c0b3
Vlad Epov <vladep622@gmail.com>
2023-11-19 17:44:37 +0200

    updated share.service with more share links, with future optionality to include additional data on share

--- src/services/share.service.js
+++ src/services/share.service.js
@@ -1,9 +1,26 @@
-const SHARE_URL = 'https://vadimep622.github.io/proj-my-team-design-challenge'
+const shareData = {
+    url: "https://vadimep622.github.io/proj-my-team-design-challenge"
+}
 
 export const shareService = {
-    getFacebookUrl
+    getFacebookUrl,
+    getPinterestUrl,
+    getTwitterUrl,
+    getLinkedinUrl
 }
 
 function getFacebookUrl() {
-    return `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(SHARE_URL)}`;
+    return `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareData.url)}`;
+}
+
+function getPinterestUrl() {
+    return `https://pinterest.com/pin/create/bookmarklet/?media=[post-img]&url=${encodeURIComponent(shareData.url)}&is_video=[is_video]&description=[post-title]`
+}
+
+function getTwitterUrl() {
+    return `https://twitter.com/share?url=${encodeURIComponent(shareData.url)}&text=[post-title]&via=[via]&hashtags=[hashtags]`
+}
+
+function getLinkedinUrl() {
+    return `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(shareData.url)}&title=[post-title]`
 }
