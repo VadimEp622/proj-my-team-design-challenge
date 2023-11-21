1b9df9f73699dc0b92e8e3a3e05bb84694af7f26
Vlad Epov <vladep622@gmail.com>
2023-11-21 18:38:53 +0200

    implemented helpers in cmp class names

--- src/cmps/_reusable/_lists/success-story-preview.jsx
+++ src/cmps/_reusable/_lists/success-story-preview.jsx
@@ -5,8 +5,8 @@ export function SuccessStoryPreview({ successStory }) {
     return (
         <section className="success-story-preview pos-rel">
             <section className="bg-pattern iconQuotes pos-abs"><img src={iconQuotes} alt="iconQuotes" /></section>
-            <p className="typography-5">{successStory.quote}</p>
-            <h3 className="typography-4 clr-teal-4">{successStory.fullName}</h3>
+            <p className="typography-5 mg-bl-st-36px">{successStory.quote}</p>
+            <h3 className="typography-4 clr-teal-4 mg-bl-ed-2px">{successStory.fullName}</h3>
             <h4 className="typography-6 italic font-weight-500">{successStory.position}</h4>
             <section className="avatar-pic"><img src={successStory.img} alt={successStory.imgName} /></section>
         </section>
