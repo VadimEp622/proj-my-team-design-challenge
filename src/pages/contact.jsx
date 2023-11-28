2da6b3d59aa063fcc6f8104187527a4d48aa1f9f
Vlad Epov <vladep622@gmail.com>
2023-11-28 10:13:45 +0200

    added contact-page skeleton structure

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,24 @@
-export function Contact(){
+import iconPerson from "../assets/images/icon-person.svg"
 
-    return(
-        <section className="contact-page">
-            <h1>Hello from Contact</h1>
+export function Contact() {
+
+    return (
+        <section className="contact-page full main-layout bg-clr-teal-0">
+            <section className="contact-container grid">
+                <section className="ask-us-list">
+                    <h1>Contact</h1>
+                    <h2>Ask us about</h2>
+                    <section className="ask-us-preview flex align-center">
+                        <section className="img-container no-grow-shrink"><img src={iconPerson} alt="iconPerson" /></section>
+                        <p>The quality of our talent network</p>
+                    </section>
+                </section>
+                <section className="contact-form">
+                    <label className="display-block" htmlFor="name">Name</label>
+                    <input className="width-100-percent" type="text" id="name" />
+                </section>
+
+            </section>
         </section>
     )
 }
