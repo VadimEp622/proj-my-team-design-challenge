71e941a5ebd14d5510362d46441a66cd0fc38926
Vlad Epov <vladep622@gmail.com>
2023-11-30 15:17:56 +0200

    added spacing to contact-page

--- src/cmps/_reusable/_lists/ask-us-list.jsx
+++ src/cmps/_reusable/_lists/ask-us-list.jsx
@@ -3,7 +3,7 @@ import { AskUsPreview } from "./ask-us-preview"
 export function AskUsList() {
 
     return (
-        <section className="ask-us-list">
+        <section className="ask-us-list flex column">
             {
                 askUsItems.map((askUsItem, index) => <AskUsPreview key={index} askUsItem={askUsItem} />)
             }
--- src/cmps/_reusable/_lists/ask-us-preview.jsx
+++ src/cmps/_reusable/_lists/ask-us-preview.jsx
@@ -1,7 +1,7 @@
 export function AskUsPreview({ askUsItem }) {
 
     return (
-        <section className="ask-us-preview flex align-center">
+        <section className="ask-us-preview flex align-center gap-23px">
             <section className="img-container no-grow-shrink"><img src={askUsItem.img} alt={askUsItem.imgAlt} /></section>
             <p className="typography-4">{askUsItem.text}</p>
         </section>
