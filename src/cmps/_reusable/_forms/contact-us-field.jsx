6380923145f118dc632936aab6b5e77c19c2a603
Vlad Epov <vladep622@gmail.com>
2023-12-03 14:56:27 +0200

    updated form spacing & italic error span.

--- src/cmps/_reusable/_forms/contact-us-field.jsx
+++ src/cmps/_reusable/_forms/contact-us-field.jsx
@@ -1,9 +1,11 @@
 export function ContactUsField({ input, meta, ...rest }) {
 
+    const isError = !meta.active && meta.touched && !input.value ? ' error' : ''
+
     return (
-        <section className={`contact-us-field${!meta.active && meta.touched && !input.value ? ' error' : ''}`}>
+        <section className={`contact-us-field${isError} mg-bl-ed-24px`}>
             <input {...input} type="text" placeholder={rest.placeholder} />
-            {meta.error && meta.touched && <span>{meta.error}</span>}
+            {meta.error && meta.touched && <span className="display-block mg-in-st-14px mg-bl-st-8px italic">{meta.error}</span>}
         </section>
     )
 }
