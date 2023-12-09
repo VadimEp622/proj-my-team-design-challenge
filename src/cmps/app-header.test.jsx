46e5a90f0bcceb43caf36440f9e42a6291e5a0fb
Vlad Epov <vladep622@gmail.com>
2023-12-09 17:38:36 +0200

    added tests for app-header

--- src/cmps/app-header.test.jsx
+++ src/cmps/app-header.test.jsx
@@ -1,21 +1,37 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { AppHeader } from "./app-header"
 import { MemoryRouter } from "react-router"
 import { Context } from "../context"
 
 describe('AppHeader', () => {
-    it('renders', () => {
-
-        const setIsSideNavbar = jest.fn()
-
-        render(
+    const customRender = ({ provideProps }) => {
+        return render(
             <MemoryRouter>
-                <Context.Provider value={{ setIsSideNavbar }}>
+                <Context.Provider {...provideProps}>
                     <AppHeader />
                 </Context.Provider>
             </MemoryRouter>
         )
+    }
 
+    const provideProps = {
+        value: {
+            isSideNavbar: false,
+            setIsSideNavbar: jest.fn()
+        }
+    }
+
+    it('renders mobile navbar', () => {
+        customRender({ provideProps })
+        const element = screen.getByAltText('Hamburger Icon')
+        expect(element).toBeInTheDocument()
+    })
+
+    it('mobile navbar button triggers setIsSideNavbar', () => {
+        customRender({ provideProps })
+        const element = screen.getByAltText('Hamburger Icon')
+        fireEvent.click(element)
+        expect(provideProps.value.setIsSideNavbar).toHaveBeenCalled()
     })
 })
 
